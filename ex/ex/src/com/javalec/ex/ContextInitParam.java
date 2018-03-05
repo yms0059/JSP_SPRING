893e162f974586838d1c28a81d79c29b8ec83283
윤명식 <yms0059@naver.com>
2018-03-05 13:33:02 +0900

    서블릿- web.xml  <context-param> <param-name>id</param-name> <param-value>12</param-value> 파라미터값 초기화

--- ex/ex/src/com/javalec/ex/ContextInitParam.java
+++ ex/ex/src/com/javalec/ex/ContextInitParam.java
@@ -4,23 +4,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ServletInitParam
+ * Servlet implementation class ContextInitParam
  */
-@WebServlet(urlPatterns={"/ServletInitParam"}, initParams={@WebInitParam(name="id", value="abcdef"), @WebInitParam(name="pw", value="1234"), @WebInitParam(name="path", value="C:\\javalec\\workspace")})
-public class ServletInitParam extends HttpServlet {
+@WebServlet("/ContextInitParam")
+public class ContextInitParam extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletInitParam() {
+    public ContextInitParam() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,16 +31,20 @@ public class ServletInitParam extends HttpServlet {
 		// TODO Auto-generated method stub
 		System.out.println("doGet");
 		
-		String id = getInitParameter("id");
-		String pw = getInitParameter("pw");
-		String path = getInitParameter("path");
+		String id = getServletContext().getInitParameter("id");
+		String pw = getServletContext().getInitParameter("pw");
+		//String path = getServletContext().getInitParameter("path");
 		
 		response.setContentType("text/html; charset=EUC-KR");
 		PrintWriter writer = response.getWriter();
+		
+		System.out.println(id);
+		System.out.println(pw);
+		
 		writer.println("<html><head></head><body>");
 		writer.println("아이디 : " + id + "<br />");
 		writer.println("비밀번호 : " + pw + "<br />");
-		writer.println("path : " + path);
+		//writer.println("path : " + path);
 		writer.println("</body></html>");
 		
 		writer.close();
